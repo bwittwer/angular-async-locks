0a44e4f72d778e0adb7fdb07792060f00e49f15b
bernard.wittwer <bernard.wittwer@leshop.ch>
2014-11-20 10:44:58 +0100

    Replace use of Number.isNaN for IE and Safari compatibility (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isNaN#Browser_compatibility)

--- dist/async-lock.js
+++ dist/async-lock.js
@@ -59,7 +59,7 @@ angular.module('boriskozo.async-locks', [])
        */
       AsyncLock.prototype.reduceQueue = function (queue, options) {
         var result = [];
-        if ((typeof options.maxQueueSize !== 'number') || Number.isNaN(options.maxQueueSize)) {
+        if ((typeof options.maxQueueSize !== 'number') || isNaN(options.maxQueueSize)) {
           return result;
         }
 
@@ -259,7 +259,7 @@ angular.module('boriskozo.async-locks', [])
        */
       ResetEvent.prototype.reduceQueue = function (queue, options) {
         var result = [];
-        if ((typeof options.maxQueueSize !== 'number') || Number.isNaN(options.maxQueueSize)) {
+        if ((typeof options.maxQueueSize !== 'number') || isNaN(options.maxQueueSize)) {
           return result;
         }
 
